750d7090389a765d6a594713c70dbfb5547768e3
neumann <neumann@192.168.1.9>
2015-03-13 21:15:54 +0100

    Init project

---
+++ 0.0.1/vo-lifecycle-core/src/main/java/org/volifecycle/lifecycle/impl/LifeCycleCheckerImpl.java
@@ -0,0 +1,52 @@
+package org.volifecycle.lifecycle.impl;
+
+import org.volifecycle.lifecycle.LifeCycleChecker;
+
+/**
+ * Impl�mentation g�n�rique du checker
+ * 
+ * @author Idriss Neumann <neumann.idriss@gmail.com>
+ *
+ * @param <T>
+ */
+public abstract class LifeCycleCheckerImpl<T> implements LifeCycleChecker<T> {
+    /**
+     * Id pour pouvoir forcer le passage du checker
+     */
+    protected String id;
+
+    /**
+     * Description
+     */
+    protected String description;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ 0.0.1/vo-lifecycle-core/src/main/java/org/volifecycle/lifecycle/impl/LifeCycleManagerImpl.java
@@ -0,0 +1,119 @@
+package org.volifecycle.lifecycle.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.volifecycle.constants.Constants;
+import org.volifecycle.lifecycle.LifeCycleAdapter;
+import org.volifecycle.lifecycle.LifeCycleManager;
+import org.volifecycle.lifecycle.LifeCycleState;
+import org.volifecycle.lifecycle.LifeCycleTransition;
+
+/**
+ * Manager des transitions
+ * 
+ * @author Idriss Neumann <neumann.idriss@gmail.com>
+ *
+ * @param <T> valueObjectType
+ */
+public class LifeCycleManagerImpl<T, A extends LifeCycleAdapter<T>> implements LifeCycleManager<T, LifeCycleAdapter<T>> {
+    /**
+     * Etats par ids
+     */
+    protected Map<String, LifeCycleState<T>> statesById;
+    protected A adapter;
+
+    /**
+     * Description
+     */
+    protected String description;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String runTransition(String idTransition, T valueObject, List<String> forcedCheckers) {
+        if (null == valueObject) {
+            throw new IllegalStateException("Objet inexistant : " + valueObject);
+        }
+
+        String keyState = adapter.getState(valueObject);
+
+        // Recherche de l'état courrant de l'objet dans la table de transco
+        if (null == keyState || !statesById.containsKey(keyState)) {
+            throw new IllegalStateException("Etat inexistant : " + keyState);
+        }
+        LifeCycleState<T> currentState = statesById.get(keyState);
+
+        // Récupération de toutes les transitions associées à l'état courrant
+        Map<String, LifeCycleTransition<T>> transitionsById = currentState.getTransitionsById();
+
+        // Recherche de la transition demandée dans les transitions autorisées
+        // pour l'object courrant
+        if (null == transitionsById || !transitionsById.containsKey(idTransition)) {
+            throw new IllegalStateException("Transition inexistante (" + idTransition + ") pour l'état " + currentState);
+        }
+
+        LifeCycleTransition<T> transition = transitionsById.get(idTransition);
+        String rtn = transition.changeState(valueObject, forcedCheckers);
+        String targetState = transition.getTarget();
+
+        // Mise à jour en bdd
+        if (null != targetState && !Constants.FALSE.equalsIgnoreCase(rtn)) {
+            adapter.setState(valueObject, targetState);
+        }
+
+        return rtn;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String runTransition(String idTransition, T valueObject) {
+        return runTransition(idTransition, valueObject, null);
+    }
+
+    /**
+     * @return the statesById
+     */
+    public Map<String, LifeCycleState<T>> getStatesById() {
+        return statesById;
+    }
+
+    /**
+     * @param statesById the statesById to set
+     */
+    public void setStatesById(Map<String, LifeCycleState<T>> statesById) {
+        this.statesById = statesById;
+    }
+
+    /**
+     * @return the adapter
+     */
+    public A getAdapter() {
+        return adapter;
+    }
+
+    /**
+     * @param adapter the adapter to set
+     */
+    public void setAdapter(A adapter) {
+        this.adapter = adapter;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
