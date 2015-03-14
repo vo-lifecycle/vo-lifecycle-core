5b5bfb10cc3be29bf36d2212083610dd484ffc4f
idrissneumann <neumann.idriss@gmail.com>
2015-03-14 12:18:10 +0100

    Unit tests dateUtils

--- 0.0.1/vo-lifecycle-core/src/main/java/org/volifecycle/lifecycle/impl/LifeCycleCheckerImpl.java
+++ 0.0.1/vo-lifecycle-core/src/main/java/org/volifecycle/lifecycle/impl/LifeCycleCheckerImpl.java
@@ -6,47 +6,50 @@ import org.volifecycle.lifecycle.LifeCycleChecker;
  * Implementation of checker
  * 
  * @author Idriss Neumann <neumann.idriss@gmail.com>
- *
- * @param <T> valueObject
+ * 
+ * @param <T>
+ *            valueObject
  */
 public abstract class LifeCycleCheckerImpl<T> implements LifeCycleChecker<T> {
-    /**
-     * Id which is used for forced the result of this checker
-     */
-    protected String id;
+	/**
+	 * Id which is used for forced the result of this checker
+	 */
+	protected String id;
 
-    /**
-     * Description
-     */
-    protected String description;
+	/**
+	 * Description
+	 */
+	protected String description;
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getId() {
-        return id;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getId() {
+		return id;
+	}
 
-    /**
-     * @param id the id to set
-     */
-    public void setId(String id) {
-        this.id = id;
-    }
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getDescription() {
-        return description;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getDescription() {
+		return description;
+	}
 
-    /**
-     * @param description the description to set
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	/**
+	 * @param description
+	 *            the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
--- 0.0.1/vo-lifecycle-core/src/main/java/org/volifecycle/transco/Transco.java
+++ 0.0.1/vo-lifecycle-core/src/main/java/org/volifecycle/transco/Transco.java
@@ -6,45 +6,44 @@ import java.util.Map;
  * Transco table interface
  * 
  * @author Idriss Neumann <neumann.idriss@gmail.com>
- *
+ * 
  */
 public interface Transco extends Map<String, String> {
-    /**
-     * Return the initial map definition
-     * 
-     * @return Map<String, String>
-     */
-    Map<String, String> getMap();
+	/**
+	 * Return the initial map definition
+	 * 
+	 * @return Map<String, String>
+	 */
+	Map<String, String> getMap();
 
-    /**
-     * Set the initial map definition
-     * 
-     * @param map
-     */
-    void setMap(Map<String, String> map);
+	/**
+	 * Set the initial map definition
+	 * 
+	 * @param map
+	 */
+	void setMap(Map<String, String> map);
 
-    /**
-     * Return the key if not found
-     * 
-     * @param key
-     * @return
-     */
-    String getQuietly(String key);
+	/**
+	 * Return the key if not found
+	 * 
+	 * @param key
+	 * @return
+	 */
+	String getQuietly(String key);
 
-    /**
-     * Return the key if not found
-     * 
-     * @param value
-     * @return
-     */
-    String getKey(String value);
+	/**
+	 * Return the key if not found
+	 * 
+	 * @param value
+	 * @return
+	 */
+	String getKey(String value);
 
-    /**
-     * Return the key if not found
-     * 
-     * @param value
-     * @return
-     */
-    String getKeyQuietly(String value);
+	/**
+	 * Return the key if not found
+	 * 
+	 * @param value
+	 * @return
+	 */
+	String getKeyQuietly(String value);
 }
-
