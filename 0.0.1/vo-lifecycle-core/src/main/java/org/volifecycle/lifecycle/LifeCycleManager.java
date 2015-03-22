d43daec3fd9c871f151b44336382164e78b9bb02
idrissneumann <neumann.idriss@gmail.com>
2015-03-22 10:55:17 +0100

    Methods getAdapter and getChangeSaver in super class

--- 0.0.1/vo-lifecycle-core/src/main/java/org/volifecycle/lifecycle/LifeCycleManager.java
+++ 0.0.1/vo-lifecycle-core/src/main/java/org/volifecycle/lifecycle/LifeCycleManager.java
@@ -6,32 +6,46 @@ import java.util.List;
  * Manager interface
  * 
  * @author Idriss Neumann <neumann.idriss@gmail.com>
- *
- * @param <T> ValueObject type
- * @param <A> Adapter type
+ * 
+ * @param <T>
+ *            ValueObject type
+ * @param <A>
+ *            Adapter type
  */
 public interface LifeCycleManager<T, A extends LifeCycleAdapter<T>> {
-    /**
-     * Run a transition on valueObject from a transition configuration id
-     * 
-     * @param transitionId
-     * @param valueObject
-     * @return String "true" or "false"
-     */
-    String runTransition(String transitionId, T valueObject);
+	/**
+	 * Run a transition on valueObject from a transition configuration id
+	 * 
+	 * @param transitionId
+	 * @param valueObject
+	 * @return String "true" or "false"
+	 */
+	String runTransition(String transitionId, T valueObject);
 
-    /**
-     * Run a transition on valueObject from a transition configuration id and forced checkers list
-     * 
-     * @param idTransition
-     * @param valueObject
-     * @param forcedCheckers
-     * @return String "true" or "false"
-     */
-    String runTransition(String idTransition, T valueObject, List<String> forcedCheckers);
-    
-    /**
-     * @return the description
-     */
-    String getDescription();
+	/**
+	 * Run a transition on valueObject from a transition configuration id and
+	 * forced checkers list
+	 * 
+	 * @param idTransition
+	 * @param valueObject
+	 * @param forcedCheckers
+	 * @return String "true" or "false"
+	 */
+	String runTransition(String idTransition, T valueObject,
+			List<String> forcedCheckers);
+
+	/**
+	 * @return the description
+	 */
+	String getDescription();
+
+	/**
+	 * @return the saver
+	 */
+	public LifeCycleChangeSaver getSaver();
+
+	/**
+	 * @return the adapter
+	 */
+	public A getAdapter();
 }
