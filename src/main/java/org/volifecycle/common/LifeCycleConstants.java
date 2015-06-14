024cd3bf1aaa46e00035891aa9762a03a86b4bcc
idrissneumann <neumann.idriss@gmail.com>
2015-06-14 22:52:43 +0200

    Preparing JSR303 validation implementation

--- src/main/java/org/volifecycle/common/LifeCycleConstants.java
+++ src/main/java/org/volifecycle/common/LifeCycleConstants.java
@@ -7,28 +7,29 @@ package org.volifecycle.common;
  * 
  */
 public interface LifeCycleConstants {
-    String TRUE = "true";
-    String FALSE = "false";
-    String AUTO = "auto";
-    String MANUAL = "manual";
-    String SYS_ACTOR = "system";
-    String EMPTY_STRING = "";
+	String TRUE = "true";
+	String FALSE = "false";
+	String AUTO = "auto";
+	String MANUAL = "manual";
+	String SYS_ACTOR = "system";
+	String EMPTY_STRING = "";
 
-    /**
-     * Types of event
-     */
-    String EVENT_TYPE_FAILURE = "failure";
-    String EVENT_TYPE_AUTH = "auth";
-    String EVENT_TYPE_OTHER = "other";
-    String EVENT_TYPE_FORCED_ACTION = "forced_action";
-    String EVENT_TYPE_FAILED_ACTION = "failed_action";
-    String EVENT_TYPE_FAILED_TRANSITION = "failed_transition";
-    String EVENT_TYPE_REFLEXION_ERROR = "reflexion_error";
-    String EVENT_TYPE_DIFF_VO = "diff_vo";
+	/**
+	 * Types of event
+	 */
+	String EVENT_TYPE_FAILURE = "failure";
+	String EVENT_TYPE_AUTH = "auth";
+	String EVENT_TYPE_OTHER = "other";
+	String EVENT_TYPE_FORCED_ACTION = "forced_action";
+	String EVENT_TYPE_FAILED_ACTION = "failed_action";
+	String EVENT_TYPE_FAILED_TRANSITION = "failed_transition";
+	String EVENT_TYPE_REFLEXION_ERROR = "reflexion_error";
+	String EVENT_TYPE_DIFF_VO = "diff_vo";
+	String EVENT_TYPE_JSR303_FAILURE = "jsr303_failure";
 
-    /**
-     * Types of diff
-     */
-    String DIFF_TYPE_VALUE = "value";
-    String DIFF_TYPE_SIZE = "size";
+	/**
+	 * Types of diff
+	 */
+	String DIFF_TYPE_VALUE = "value";
+	String DIFF_TYPE_SIZE = "size";
 }
--- src/main/java/org/volifecycle/lifecycle/impl/LifeCycleSimpleActionImpl.java
+++ src/main/java/org/volifecycle/lifecycle/impl/LifeCycleSimpleActionImpl.java
@@ -13,66 +13,66 @@ import org.volifecycle.lifecycle.LifeCycleAction;
  *            the value object type
  */
 public abstract class LifeCycleSimpleActionImpl<T> implements LifeCycleAction<T> {
-    /**
-     * Id which is used for forced the result of this action
-     */
-    protected String id;
+	/**
+	 * Id which is used for forced the result of this action
+	 */
+	protected String id;
 
-    /**
-     * Description
-     */
-    protected String description;
+	/**
+	 * Description
+	 */
+	protected String description;
 
-    /**
-     * Additionnal informations to save.
-     */
-    protected Map<String, String> additionnalInformations;
+	/**
+	 * Additionnal informations to save.
+	 */
+	protected Map<String, String> additionnalInformations;
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getId() {
-        return id;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getId() {
+		return id;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String getDescription() {
-        return description;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getDescription() {
+		return description;
+	}
 
-    /**
-     * @param id
-     *            the id to set
-     */
-    public void setId(String id) {
-        this.id = id;
-    }
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    /**
-     * @param description
-     *            the description to set
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	/**
+	 * @param description
+	 *            the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    /**
-     * @param additionnalInformations
-     *            the additionnalInformations to set
-     */
-    public void setAdditionnalInformations(Map<String, String> additionnalInformations) {
-        this.additionnalInformations = additionnalInformations;
-    }
+	/**
+	 * @param additionnalInformations
+	 *            the additionnalInformations to set
+	 */
+	public void setAdditionnalInformations(Map<String, String> additionnalInformations) {
+		this.additionnalInformations = additionnalInformations;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Map<String, String> getAdditionnalInformations() {
-        return additionnalInformations;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Map<String, String> getAdditionnalInformations() {
+		return additionnalInformations;
+	}
 }
