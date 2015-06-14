d6b8be15fdf3209e4cc2eae02d9f4bc53e9a4dee
idrissneumann <neumann.idriss@gmail.com>
2015-06-14 23:49:57 +0200

    Implementing lifecycle synchronized process.

---
+++ src/main/java/org/volifecycle/lifecycle/LifeCycleSynchronizedProcess.java
@@ -0,0 +1,33 @@
+package org.volifecycle.lifecycle;
+
+import java.util.List;
+
+import org.volifecycle.lifecycle.impl.LifeCycleManagerImpl;
+
+/**
+ * LifeCycle process is used to launch multiple transitions.
+ * 
+ * @author Idriss Neumann <neumann.idriss@gmail.com>
+ *
+ * @param <T>
+ *            : value object's type
+ */
+public interface LifeCycleSynchronizedProcess<T> {
+	/**
+	 * @return the manager
+	 */
+	LifeCycleManagerImpl<T, LifeCycleAdapter<T>> getManager();
+
+	/**
+	 * @return the transitionIds
+	 */
+	List<String> getTransitionIds();
+
+	/**
+	 * Launching a list of transition on a value object
+	 * 
+	 * @param valueObject
+	 * @return the last state if success or "false"
+	 */
+	String process(T valueObject);
+}
---
+++ src/main/java/org/volifecycle/lifecycle/impl/LifeCycleSynchronizedProcessImpl.java
@@ -0,0 +1,68 @@
+package org.volifecycle.lifecycle.impl;
+
+import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
+
+import java.util.List;
+
+import org.volifecycle.common.LifeCycleConstants;
+import org.volifecycle.lifecycle.LifeCycleAdapter;
+import org.volifecycle.lifecycle.LifeCycleSynchronizedProcess;
+
+/**
+ * LifeCycleSynchronizedProcess implementation.
+ * 
+ * @author Idriss Neumann <neumann.idriss@gmail.com>
+ *
+ * @param <T>
+ *            : value object's type
+ */
+public class LifeCycleSynchronizedProcessImpl<T> implements LifeCycleSynchronizedProcess<T> {
+	protected LifeCycleManagerImpl<T, LifeCycleAdapter<T>> manager;
+	protected List<String> transitionIds;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public LifeCycleManagerImpl<T, LifeCycleAdapter<T>> getManager() {
+		return manager;
+	}
+
+	/**
+	 * @param manager
+	 *            the manager to set
+	 */
+	public void setManager(LifeCycleManagerImpl<T, LifeCycleAdapter<T>> manager) {
+		this.manager = manager;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<String> getTransitionIds() {
+		return transitionIds;
+	}
+
+	/**
+	 * @param transitionIds
+	 *            the transitionIds to set
+	 */
+	public void setTransitionIds(List<String> transitionIds) {
+		this.transitionIds = transitionIds;
+	}
+
+	@Override
+	public String process(T valueObject) {
+		String rtn = LifeCycleConstants.FALSE;
+		if (isNotEmpty(transitionIds)) {
+			for (String transitionId : transitionIds) {
+				rtn = manager.runTransition(transitionId, valueObject);
+				if (LifeCycleConstants.FALSE.equalsIgnoreCase(rtn)) {
+					break;
+				}
+			}
+		}
+		return rtn;
+	}
+}
