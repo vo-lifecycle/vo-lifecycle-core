3980cbc408a18ba207d1018b155a38425ea5b06f
Idriss Neumann <idriss.neumann@capgemini.com>
2015-07-21 11:30:22 +0200

    Fix : error msg

--- src/main/java/org/volifecycle/lifecycle/impl/LifeCycleManagerImpl.java
+++ src/main/java/org/volifecycle/lifecycle/impl/LifeCycleManagerImpl.java
@@ -96,7 +96,7 @@ public class LifeCycleManagerImpl<T, A extends LifeCycleAdapter<T>> implements L
 
         // Searching current state in transco
         if (null == keyState || !statesById.containsKey(keyState)) {
-            throw new IllegalStateException("The transition is not applicable for the state " + ((null == keyState) ? "<null>" : keyState));
+            throw new IllegalStateException("The transition " + idTransition + " is not applicable for the state " + ((null == keyState) ? "<null>" : keyState));
         }
 
         LifeCycleState<T> currentState = statesById.get(keyState);
