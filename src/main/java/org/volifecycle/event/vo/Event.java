8c65cb07bf0a277fc33e2f24ce2947d1306b9c32
Idriss Neumann <idriss.neumann@docapost.fr>
2017-02-03 18:39:59 +0100

    Modernize a little bit of code

--- src/main/java/org/volifecycle/event/vo/Event.java
+++ src/main/java/org/volifecycle/event/vo/Event.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Event value object class
@@ -12,165 +13,213 @@ import java.util.Map;
  * 
  */
 public class Event implements Serializable {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    /**
-     * Type of event (issue, etc)
-     */
-    protected String typeEvent;
+  /**
+   * Type of event (issue, etc)
+   */
+  protected String typeEvent;
 
-    /**
-     * when ?
-     */
-    protected Calendar date;
+  /**
+   * when ?
+   */
+  protected Calendar date;
 
-    /**
-     * what ?
-     */
-    protected String details;
+  /**
+   * what ?
+   */
+  protected String details;
 
-    /**
-     * who ?
-     */
-    protected String actor;
+  /**
+   * who ?
+   */
+  protected String actor;
 
-    /**
-     * Value object impacted
-     */
-    protected String idValueObject;
+  /**
+   * Value object impacted
+   */
+  protected String idValueObject;
 
-    /**
-     * Type of value object impacted
-     */
-    protected String typeValueObject;
+  /**
+   * Type of value object impacted
+   */
+  protected String typeValueObject;
 
-    /**
-     * List of failed action's ids
-     */
-    protected List<String> failedActionsIds;
+  /**
+   * List of failed action's ids
+   */
+  protected List<String> failedActionsIds;
 
-    /**
-     * Additional informations (like comments, sources, etc)
-     */
-    protected Map<String, String> additionalInformations;
+  /**
+   * Additional informations (like comments, sources, etc)
+   */
+  protected Map<String, String> additionalInformations;
 
-    /**
-     * @return the typeEvent
-     */
-    public String getTypeEvent() {
-        return typeEvent;
+  /**
+   * @return the typeEvent
+   */
+  public String getTypeEvent() {
+    return typeEvent;
+  }
+
+  /**
+   * @param typeEvent the typeEvent to set
+   */
+  public void setTypeEvent(String typeEvent) {
+    this.typeEvent = typeEvent;
+  }
+
+  /**
+   * @return the date
+   */
+  public Calendar getDate() {
+    return date;
+  }
+
+  /**
+   * @param date the date to set
+   */
+  public void setDate(Calendar date) {
+    this.date = date;
+  }
+
+  /**
+   * @return the details
+   */
+  public String getDetails() {
+    return details;
+  }
+
+  /**
+   * @param details the details to set
+   */
+  public void setDetails(String details) {
+    this.details = details;
+  }
+
+  /**
+   * @return the actor
+   */
+  public String getActor() {
+    return actor;
+  }
+
+  /**
+   * @param actor the actor to set
+   */
+  public void setActor(String actor) {
+    this.actor = actor;
+  }
+
+  /**
+   * @return the idValueObject
+   */
+  public String getIdValueObject() {
+    return idValueObject;
+  }
+
+  /**
+   * @param idValueObject the idValueObject to set
+   */
+  public void setIdValueObject(String idValueObject) {
+    this.idValueObject = idValueObject;
+  }
+
+  /**
+   * @return the typeValueObject
+   */
+  public String getTypeValueObject() {
+    return typeValueObject;
+  }
+
+  /**
+   * @param typeValueObject the typeValueObject to set
+   */
+  public void setTypeValueObject(String typeValueObject) {
+    this.typeValueObject = typeValueObject;
+  }
+
+  /**
+   * @return the additionalInformations
+   */
+  public Map<String, String> getAdditionalInformations() {
+    return additionalInformations;
+  }
+
+  /**
+   * @param additionalInformations the additionalInformations to set
+   */
+  public void setAdditionalInformations(Map<String, String> additionalInformations) {
+    this.additionalInformations = additionalInformations;
+  }
+
+  /**
+   * @return the failedActionsIds
+   */
+  public List<String> getFailedActionsIds() {
+    return failedActionsIds;
+  }
+
+  /**
+   * @param failedActionsIds the failedActionsIds to set
+   */
+  public void setFailedActionsIds(List<String> failedActionsIds) {
+    this.failedActionsIds = failedActionsIds;
+  }
+
+  public static class Builder {
+    Event event;
+
+    public Builder() {
+      event = new Event();
     }
 
-    /**
-     * @param typeEvent
-     *            the typeEvent to set
-     */
-    public void setTypeEvent(String typeEvent) {
-        this.typeEvent = typeEvent;
+    public Builder(Event event) {
+      this.event = Objects.requireNonNull(event, "event must not be null");
     }
 
-    /**
-     * @return the date
-     */
-    public Calendar getDate() {
-        return date;
+    public Builder typeEvent(String typeEvent) {
+      event.setTypeEvent(typeEvent);
+      return this;
     }
 
-    /**
-     * @param date
-     *            the date to set
-     */
-    public void setDate(Calendar date) {
-        this.date = date;
+    public Builder date(Calendar date) {
+      event.setDate(date);
+      return this;
     }
 
-    /**
-     * @return the details
-     */
-    public String getDetails() {
-        return details;
+    public Builder details(String details) {
+      event.setDetails(details);
+      return this;
     }
 
-    /**
-     * @param details
-     *            the details to set
-     */
-    public void setDetails(String details) {
-        this.details = details;
+    public Builder actor(String actor) {
+      event.setActor(actor);
+      return this;
     }
 
-    /**
-     * @return the actor
-     */
-    public String getActor() {
-        return actor;
+    public Builder idValueObject(String idValueObject) {
+      event.setIdValueObject(idValueObject);
+      return this;
     }
 
-    /**
-     * @param actor
-     *            the actor to set
-     */
-    public void setActor(String actor) {
-        this.actor = actor;
+    public Builder typeValueObject(String typeValueObject) {
+      event.setTypeValueObject(typeValueObject);
+      return this;
     }
 
-    /**
-     * @return the idValueObject
-     */
-    public String getIdValueObject() {
-        return idValueObject;
+    public Builder failedActionsIds(List<String> failedActionsIds) {
+      event.setFailedActionsIds(failedActionsIds);
+      return this;
     }
 
-    /**
-     * @param idValueObject
-     *            the idValueObject to set
-     */
-    public void setIdValueObject(String idValueObject) {
-        this.idValueObject = idValueObject;
+    public Builder additionalInformations(Map<String, String> additionalInformations) {
+      event.setAdditionalInformations(additionalInformations);
+      return this;
     }
 
-    /**
-     * @return the typeValueObject
-     */
-    public String getTypeValueObject() {
-        return typeValueObject;
+    public Event build() {
+      return event;
     }
-
-    /**
-     * @param typeValueObject
-     *            the typeValueObject to set
-     */
-    public void setTypeValueObject(String typeValueObject) {
-        this.typeValueObject = typeValueObject;
-    }
-
-    /**
-     * @return the additionalInformations
-     */
-    public Map<String, String> getAdditionalInformations() {
-        return additionalInformations;
-    }
-
-    /**
-     * @param additionalInformations
-     *            the additionalInformations to set
-     */
-    public void setAdditionalInformations(Map<String, String> additionalInformations) {
-        this.additionalInformations = additionalInformations;
-    }
-
-    /**
-     * @return the failedActionsIds
-     */
-    public List<String> getFailedActionsIds() {
-        return failedActionsIds;
-    }
-
-    /**
-     * @param failedActionsIds
-     *            the failedActionsIds to set
-     */
-    public void setFailedActionsIds(List<String> failedActionsIds) {
-        this.failedActionsIds = failedActionsIds;
-    }
+  }
 }
